0d53446d58eb3053444170f51a3eb599e04040d8
yuki <c0a2210212@edu.teu.ac.jp>
2023-04-18 16:58:25 +0900

    sixth commit

--- pygame_image.py
+++ pygame_image.py
@@ -9,20 +9,24 @@ def main():
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
+    bg_imgs = [bg_img, pg.transform.flip(bg_img, True, False), bg_img]
     tmr = 0
     cnt = 0
+    i = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
         
+       
+        x = tmr%3200
         tmr += 1
-        if tmr % 10 == 1:    
+        if tmr % 20 == 1:    
             cnt += 1
             kk_t = cnt % 2
-        x = tmr%1600
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(bg_imgs[0], [-x, 0])
+        screen.blit(bg_imgs[1], [1600-x, 0])
+        screen.blit(bg_imgs[2], [3200-x, 0])
         screen.blit(kk_imgs[kk_t], [300, 200])
         pg.display.update()
         clock.tick(100)
