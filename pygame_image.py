b0aeeb397241cd25528dc4406fa09879e16c5b2e
yuki <c0a2210212@edu.teu.ac.jp>
2023-04-18 15:31:26 +0900

    second commit

--- pygame_image.py
+++ pygame_image.py
@@ -6,7 +6,9 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-
+    kk_img = pg.image.load("ex01/fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
     tmr = 0
 
     while True:
