c3886c6560075947a20793584fc977470e7c8318
yuki <c0a2210212@edu.teu.ac.jp>
2023-04-18 15:37:42 +0900

    fourth commit

--- pygame_image.py
+++ pygame_image.py
@@ -8,7 +8,7 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
     tmr = 0
 
     while True:
